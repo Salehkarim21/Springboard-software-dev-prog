cfc4c54239980268260e55491886d3fa5831ef8b
Salehkarim21 <salehkarim1028@gmail.com>
2026-06-01 21:10:57 -0500

    cesar 1

---
+++ Cesar Project/EX 1/EX 1 Q3.js
@@ -0,0 +1,10 @@
+let partyLocation = "GARDEN"  //let because we need to shift back to this word.
+
+const shiftValue = 3   //shift value is fixed.
+
+let encryptedMessage = "JDUGHQ" //message will change after decryption.
+
+let decryptedMessage = "GARDEN" //message will change after encryption.
+
+let crasherFree = true // might be true or false based on whether the crashers deciphered the code.
+

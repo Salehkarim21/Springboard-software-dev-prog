b8b18a513b85f90c4a48ddf8a993777aa13b7174
Salehkarim21 <salehkarim1028@gmail.com>
2026-06-09 22:45:12 -0500

    5th caesar

---
+++ Cesar Project/EX 5/EX 5.js
@@ -0,0 +1,21 @@
+const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+
+guests.unshift("BRUTUS"); //will add BRUTUS to the beginning of the array.
+
+guests.push("AUGUSTUS", "LUCIA"); //will add AUGUSTUS and LUCIA to the end of the array.
+
+guests.includes("SPARTACUS"); //will check if SPARTACUS is in the array and return true or false.
+
+const spartacusIndex = guests.indexOf("SPARTACUS"); //will return -1 since "SPARTACUS" it is not found in the array.
+
+const updatedGuests = guests.splice(3,1); //will remove the element at index 3, which is "CASSIUS".
+
+const firstFourGuests = guests.slice(0,3); //will create a new array with the first three guests, which are "BRUTUS", "ANTONY", and "CICERO".
+
+const honoredGuests = guests.slice(0, 1); // will extract the honored guests.
+
+const unhonoredGuests = guests.slice(1); // will extract the rest of the guests.
+
+unhonoredGuests.sort(); // Sorts the unhonored guests.
+
+const sortedGuests = honoredGuests.concat(unhonoredGuests); // ro combines both arrays arrange the way Caesar wanted it with Honored guest at the begining. 

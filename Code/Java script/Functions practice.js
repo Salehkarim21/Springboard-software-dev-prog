293f4a957ec16a8be89deb0ebc8b2f8bde57925e
Salehkarim21 <salehkarim1028@gmail.com>
2026-06-21 23:46:52 -0500

    ff

--- Code/Java script/Functions practice.js
+++ Code/Java script/Functions practice.js
@@ -11,3 +11,52 @@
 // isValidPassword('dogLuvr123!', 'dogLuvr') //false
 // isValidPassword('hello1', 'dogLuvr') //false
 
+f
+SOLUTION 
+
+function isValidPassword(password, username) {
+	if (password.length < 8) {
+		return false;
+	}
+	if (password.indexOf(' ') !== -1) {
+		return false;
+	}
+	if (password.indexOf(username) !== -1) {
+		return false;
+	}
+	return true;
+}
+
+function isValidPassword(password, username) {
+	if (
+		password.length < 8 ||
+		password.indexOf(' ') !== -1 ||
+		password.indexOf(username) !== -1
+	) {
+		return false;
+	}
+	return true;
+}
+
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	if (tooShort || hasSpace || tooSimilar) return false;
+	return true;
+}
+
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	if (!tooShort && !hasSpace && !tooSimilar) return true;
+	return false;
+}
+
+function isValidPassword(password, username) {
+	const tooShort = password.length < 8;
+	const hasSpace = password.indexOf(' ') !== -1;
+	const tooSimilar = password.indexOf(username) !== -1;
+	return !tooShort && !hasSpace && !tooSimilar;
+}

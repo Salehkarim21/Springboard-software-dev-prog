9055786eeae04305e8d1228f3ec70e54a404321e
Salehkarim21 <salehkarim1028@gmail.com>
2026-07-24 23:09:20 -0500

    es

---
+++ Advanced Array Projects/ES2015/ES5 code.js
@@ -0,0 +1,22 @@
+
+
+var PI = 3.14;
+const PI = 42; // use const here since we need a constant value for PI
+
+## **Quiz**
+
+1- What is the difference between var and let?
+
+    we can reassign a value and redecalare values with var but let allows us to reassign values but not redeclare them. Also, var is function scoped while let is block scoped.
+
+2- What is the difference between var and const?
+
+    var allows us to reassign and redeclare values, while const does not allow either. Also, var is function scoped while const is block scoped.
+
+3- What is the difference between let and const?
+
+    let allows us to reassign values but not redeclare them, while const does not allow either. Also, let is block scoped while const is block scoped.
+
+4- What is hoisting?
+
+    Hoisting is the behavior of moving variable and function declarations to the top of their scope before code execution.

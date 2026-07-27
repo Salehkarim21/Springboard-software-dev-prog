445c1a7cf61ca93af3d35e8cddc4dd8aeb2fa5f4
Salehkarim21 <salehkarim1028@gmail.com>
2026-07-26 21:02:33 -0500

    arrow

---
+++ ES2015/Arrow Function.js
@@ -0,0 +1,34 @@
+
+
+Without Arrow Function
+
+
+function double(arr) {
+  return arr.map(function(val) {
+    return val * 2;
+  });
+}
+
+
+
+With Arrow Function
+
+const double = arr => arr.map(val => val * 2);
+
+
+Without Arrow Function
+
+function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+}
+
+
+With Arrow Function
+
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0)
